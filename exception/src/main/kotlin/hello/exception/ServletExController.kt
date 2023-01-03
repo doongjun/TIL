e70442a76a7ca3cb457536b7b74519b7193ca48a
doongjun <doongjun@kakao.com>
2023-01-03 17:54:01 +0900

    exception log

---
+++ exception/src/main/kotlin/hello/exception/ServletExController.kt
@@ -0,0 +1,23 @@
+package hello.exception
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import javax.servlet.http.HttpServletResponse
+
+@Controller
+class ServletExController {
+    @GetMapping("/error-ex")
+    fun errorEx() {
+        throw RuntimeException("예외 발생!")
+    }
+
+    @GetMapping("/error-404")
+    fun error404(response: HttpServletResponse) {
+        response.sendError(404, "404 오류")
+    }
+
+    @GetMapping("/error-500")
+    fun error500(response: HttpServletResponse) {
+        response.sendError(500)
+    }
+}
