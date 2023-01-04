563c1ab01b9f87f5266c0578544de80a20d9eb9a
doongjun <doongjun@kakao.com>
2023-01-04 14:42:56 +0900

    spring boot api error

---
+++ exception/src/main/kotlin/hello/exception/ApiExceptionController.kt
@@ -0,0 +1,25 @@
+package hello.exception
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class ApiExceptionController {
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    @GetMapping("/api/members/{id}")
+    fun getMember(@PathVariable id: String): MemberDto {
+        if (id == "ex") {
+            throw RuntimeException("잘못된 사용자")
+        }
+
+        return MemberDto(id, "hello $id")
+    }
+}
+
+data class MemberDto(
+    val memberId: String,
+    val name: String
+)
