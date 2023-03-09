7663f6d9c99f933b99b5a6c1f7ada624492a2c8d
doongjun <doongjun.kim@gmail.com>
2023-03-09 23:56:21 +0900

    view converter

--- typeconverter/src/main/kotlin/hello/typeconverter/converter/IpPortToStringConverter.kt
+++ typeconverter/src/main/kotlin/hello/typeconverter/converter/IpPortToStringConverter.kt
@@ -1,10 +1,13 @@
 package hello.typeconverter.converter
 
 import hello.typeconverter.type.IpPort
+import org.slf4j.LoggerFactory
 import org.springframework.core.convert.converter.Converter
 
 class IpPortToStringConverter: Converter<IpPort, String> {
+    private val log = LoggerFactory.getLogger(javaClass)
     override fun convert(source: IpPort): String {
+        log.info("convert source={}", source)
         return "${source.ip}:${source.port}"
     }
 }
