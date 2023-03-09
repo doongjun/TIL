90a1b3664480b6b55191f79677ae11faa7d38fa3
doongjun <doongjun@kakao.com>
2023-03-09 23:11:08 +0900

    type converter

--- typeconverter/src/test/kotlin/hello/typeconverter/converter/ConverterTest.kt
+++ typeconverter/src/test/kotlin/hello/typeconverter/converter/ConverterTest.kt
@@ -1,5 +1,6 @@
 package hello.typeconverter.converter
 
+import hello.typeconverter.type.IpPort
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -17,6 +18,22 @@ class ConverterTest {
         val converter = StringToIntegerConverter()
         val result = converter.convert("10")
         assertThat(result).isEqualTo(10)
+    }
+
+    @Test
+    fun ipPortToString() {
+        val converter = IpPortToStringConverter()
+        val source = IpPort("127.0.0.1", 8080)
+        val result = converter.convert(source)
+        assertThat(result).isEqualTo("127.0.0.1:8080")
+    }
+
+    @Test
+    fun stringToIpPort() {
+        val converter = StringToIpPortConverter()
+        val source = "127.0.0.1:8080"
+        val result = converter.convert(source)
+        assertThat(result).isEqualTo(IpPort("127.0.0.1", 8080))
     }
 
 }
