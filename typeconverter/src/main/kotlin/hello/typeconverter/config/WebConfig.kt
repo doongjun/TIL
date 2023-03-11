0d97dadd10a17ddf6af4dab15a93da407d38169e
doongjun <doongjun.kim@gmail.com>
2023-03-11 15:21:03 +0900

    config

--- typeconverter/src/main/kotlin/hello/typeconverter/config/WebConfig.kt
+++ typeconverter/src/main/kotlin/hello/typeconverter/config/WebConfig.kt
@@ -4,6 +4,7 @@ import hello.typeconverter.converter.IntegerToStringConverter
 import hello.typeconverter.converter.IpPortToStringConverter
 import hello.typeconverter.converter.StringToIntegerConverter
 import hello.typeconverter.converter.StringToIpPortConverter
+import hello.typeconverter.formatter.MyNumberFormatter
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -11,9 +12,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig: WebMvcConfigurer {
     override fun addFormatters(registry: FormatterRegistry) {
-        registry.addConverter(StringToIntegerConverter())
-        registry.addConverter(IntegerToStringConverter())
         registry.addConverter(StringToIpPortConverter())
         registry.addConverter(IpPortToStringConverter())
+
+        /**
+         * 우선순위
+         * converter > formatter
+         */
+//        registry.addConverter(StringToIntegerConverter())
+//        registry.addConverter(IntegerToStringConverter())
+        registry.addFormatter(MyNumberFormatter())
     }
 }
