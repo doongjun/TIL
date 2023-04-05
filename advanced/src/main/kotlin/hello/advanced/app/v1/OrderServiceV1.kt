e12856684814927fb12993ea4dc5c5f6d69ff765
doongjun <doongjun@kakao.com>
2023-04-05 23:32:28 +0900

    feat: v1

---
+++ advanced/src/main/kotlin/hello/advanced/app/v1/OrderServiceV1.kt
@@ -0,0 +1,23 @@
+package hello.advanced.app.v1
+
+import hello.advanced.trace.TraceStatus
+import hello.advanced.trace.hellotrace.HelloTraceV1
+import org.springframework.stereotype.Service
+
+@Service
+class OrderServiceV1(
+    private val trace: HelloTraceV1,
+    private val orderRepositoryV1: OrderRepositoryV1
+) {
+    fun orderItem(itemId: String) {
+        var status: TraceStatus? = null
+        try {
+            status = trace.begin("OrderServiceV1.request()")
+            orderRepositoryV1.save(itemId)
+            trace.end(status)
+        } catch (e: Exception) {
+            status?.let { trace.exception(it, e) }
+            throw e
+        }
+    }
+}
