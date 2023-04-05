5b9636510f946194fb688bcf88dc3f0f5d1f59fa
doongjun <doongjun@kakao.com>
2023-04-05 23:48:11 +0900

    feat: v2

--- advanced/src/main/kotlin/hello/advanced/app/v2/OrderControllerV2.kt
+++ advanced/src/main/kotlin/hello/advanced/app/v2/OrderControllerV2.kt
@@ -1,21 +1,21 @@
-package hello.advanced.app.v1
+package hello.advanced.app.v2
 
 import hello.advanced.trace.TraceStatus
-import hello.advanced.trace.hellotrace.HelloTraceV1
+import hello.advanced.trace.hellotrace.HelloTraceV2
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class OrderControllerV1(
-    private val trace: HelloTraceV1,
-    private val orderService: OrderServiceV1
+class OrderControllerV2(
+    private val trace: HelloTraceV2,
+    private val orderService: OrderServiceV2
 ) {
-    @GetMapping("/v1/request")
+    @GetMapping("/v2/request")
     fun request(itemId: String): String {
         var status: TraceStatus? = null
         try {
             status = trace.begin("OrderController.request()")
-            orderService.orderItem(itemId)
+            orderService.orderItem(status.traceId, itemId)
             trace.end(status)
             return "ok"
         } catch (e: Exception) {
---
+++ advanced/src/main/kotlin/hello/advanced/app/v2/OrderServiceV2.kt
@@ -0,0 +1,24 @@
+package hello.advanced.app.v2
+
+import hello.advanced.trace.TraceId
+import hello.advanced.trace.TraceStatus
+import hello.advanced.trace.hellotrace.HelloTraceV2
+import org.springframework.stereotype.Service
+
+@Service
+class OrderServiceV2(
+    private val trace: HelloTraceV2,
+    private val orderRepository: OrderRepositoryV2
+) {
+    fun orderItem(traceId: TraceId, itemId: String) {
+        var status: TraceStatus? = null
+        try {
+            status = trace.beginSync(traceId, "OrderService.request()")
+            orderRepository.save(status.traceId, itemId)
+            trace.end(status)
+        } catch (e: Exception) {
+            status?.let { trace.exception(it, e) }
+            throw e
+        }
+    }
+}
