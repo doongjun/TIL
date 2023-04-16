a6510e23014dd7f6f5c53b02500e10f72a48f903
doongjun <doongjun@kakao.com>
2023-04-16 18:39:01 +0900

    feat: apply bean thread local loh trace

--- advanced/src/main/kotlin/hello/advanced/trace/LogTraceConfig.kt
+++ advanced/src/main/kotlin/hello/advanced/trace/LogTraceConfig.kt
@@ -1,7 +1,7 @@
 package hello.advanced.trace
 
-import hello.advanced.trace.logtrace.FieldLogTrace
 import hello.advanced.trace.logtrace.LogTrace
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,6 +10,6 @@ class LogTraceConfig {
 
     @Bean
     fun logTrace(): LogTrace {
-        return FieldLogTrace()
+        return ThreadLocalLogTrace()
     }
 }
