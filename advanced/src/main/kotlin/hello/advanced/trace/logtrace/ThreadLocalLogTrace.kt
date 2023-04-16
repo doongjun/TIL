f15ceb99965b7991870d5c4fa2b151f6939b098a
doongjun <doongjun.kim@gmail.com>
2023-04-16 18:28:22 +0900

    feat: thread local log trace

---
+++ advanced/src/main/kotlin/hello/advanced/trace/logtrace/ThreadLocalLogTrace.kt
@@ -0,0 +1,73 @@
+package hello.advanced.trace.logtrace
+
+import hello.advanced.trace.TraceId
+import hello.advanced.trace.TraceStatus
+import org.slf4j.LoggerFactory
+
+class ThreadLocalLogTrace: LogTrace {
+    companion object {
+        private const val START_PREFIX = "-->"
+        private const val COMPLETE_PREFIX = "<--"
+        private const val EX_PREFIX = "<X-"
+    }
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    private var traceIdHolder: ThreadLocal<TraceId> = ThreadLocal()
+
+    override fun begin(message: String): TraceStatus {
+        syncTraceId()
+        val traceId = traceIdHolder.get()
+        val startTimeMs = System.currentTimeMillis()
+        log.info("[{}] {}{}", traceId.id, addSpace(START_PREFIX, traceId.level), message)
+        return TraceStatus(traceId, startTimeMs, message)
+    }
+
+    private fun syncTraceId() {
+        val traceId = traceIdHolder.get()
+        if (traceId == null) {
+            traceIdHolder.set(TraceId())
+        } else {
+            traceIdHolder.set(traceId.createNextId())
+        }
+    }
+
+    override fun end(status: TraceStatus) {
+        complete(status, null)
+    }
+
+    override fun exception(status: TraceStatus, e: Exception) {
+        complete(status, e)
+    }
+
+    private fun complete(status: TraceStatus, e: Exception?) {
+        val stopTimeMs = System.currentTimeMillis()
+        val resultTimeMs = stopTimeMs - status.startTimeMs
+        val traceId = status.traceId
+
+        if (e == null) {
+            log.info("[{}] {}{} time={}ms", traceId.id, addSpace(COMPLETE_PREFIX, traceId.level), status.message, resultTimeMs)
+        } else {
+            log.info("[{}] {}{} time={}ms ex={}", traceId.id, addSpace(EX_PREFIX, traceId.level), status.message, resultTimeMs, e.javaClass)
+        }
+
+        releaseTraceId()
+    }
+
+    private fun releaseTraceId() {
+        val traceId = traceIdHolder.get()
+        if (traceId.isFirstLevel()) {
+            traceIdHolder.remove()
+        } else {
+            traceIdHolder.set(traceId.createPreviousId())
+        }
+    }
+
+    private fun addSpace(prefix: String, level: Int): String {
+        var space = ""
+        for (i in 0 until level) {
+            space += if (level-1 == i) "|$prefix" else "|  "
+        }
+        return space
+    }
+}
