be07d82a24908b2d06cac2897cf7eafb71cb3b42
doongjun <doongjun.kim@gmail.com>
2023-04-21 21:02:15 +0900

    test: template method

--- advanced/src/test/kotlin/hello/advanced/template/TemplateMethodTest.kt
+++ advanced/src/test/kotlin/hello/advanced/template/TemplateMethodTest.kt
@@ -6,6 +6,7 @@ import hello.advanced.template.code.SubClassLogic2
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 
+
 class TemplateMethodTest {
     private val log = LoggerFactory.getLogger(javaClass)
 
@@ -44,6 +45,23 @@ class TemplateMethodTest {
         template1.execute()
 
         val template2: AbstractTemplate = SubClassLogic2()
+        template2.execute()
+    }
+
+    @Test
+    fun templateMethodV2() {
+        val template1 = object : AbstractTemplate() {
+            override fun call() {
+                log.info("비즈니스 로직1 실행")
+            }
+        }
+        template1.execute()
+
+        val template2 = object : AbstractTemplate() {
+            override fun call() {
+                log.info("비즈니스 로직2 실행")
+            }
+        }
         template2.execute()
     }
 }
